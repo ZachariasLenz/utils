8a604a0915a079c1718fbc679b64da0b280ff5ad
Zacharias Lenz <zacharias.lenz@pm.me>
2021-04-29 15:23:43 +0200

    Add neovim nightly to update script

--- utils/update_all.sh
+++ utils/update_all.sh
@@ -1,20 +1,35 @@
-# Update apt packages
+echo "Updating apt packages..."
 sudo apt update
 sudo apt upgrade
 sudo apt autoremove
+echo
 
-# Update all flatpaks
+echo "Updating flatpak packages..."
 flatpak update
+echo
 
-# Update npm
+echo "Updating npm packages..."
 npm update
+echo
 
-# Update snaps
+echo "Updating snap packages..."
 sudo snap refresh
+echo
 
-# Update nix packages
+echo "Updating nix packages..."
 nix-env -u
 nix-env -if https://github.com/srid/neuron/archive/master.tar.gz
+echo
 
-# Update spotifyd
+echo "Updating spotifyd..."
 download-spotify
+echo
+
+echo "Updating Neovim..."
+_CWD=$PWD
+cd ~/.local/bin/
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+chmod u+x nvim.appimage
+mv nvim.appimage nvim
+cd $_CWD
+echo "Done!"
